637592b10295d234245e68b09287a2cba61f7833
AnanthuTD <ananthutd2021@gmail.com>
2025-05-01 04:11:52 +0530

    fix

--- src/app/services/implementation/VideoServiceClient.ts
+++ src/app/services/implementation/VideoServiceClient.ts
@@ -33,7 +33,7 @@ export class VideoServiceClient implements IVideoServiceClient {
     };
 
     this.videoServiceBreaker = new CircuitBreaker(callVideoService, {
-      timeout: 2000,
+      timeout: 100000,
       errorThresholdPercentage: 50,
       resetTimeout: 30000,
     });
